5376675569735870effb45eb56a4397ebfdb5c19
Andrew Dawson <ajdawson@acm.org>
2018-12-05 13:13:13 +0000

    Fix sidebar table of contents

--- doc/conf.py
+++ doc/conf.py
@@ -149,7 +149,10 @@ html_static_path = ['_static']
 #html_use_smartypants = True
 
 # Custom sidebar templates, maps document names to template names.
-#html_sidebars = {}
+html_sidebars = {'**': ['sidebar_toc.html',
+                        'relations.html',
+                        'sourcelink.html',
+                        'searchbox.html']}
 
 # Additional templates that should be rendered to pages, maps page names to
 # template names.
