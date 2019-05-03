d6889e4f8579c79122f47a613525a246ee465ee5
Doctr (Travis CI) <drdoctr@users.noreply.github.com>
2019-05-03 10:47:59 +0000

    Update docs after building Travis build 334 of ajdawson/eofs
    The docs were built from the branch 'master' against the commit
    402ab3889adf66176a8aece5266627788a667b44.
    
    The Travis build that generated this commit is at
    https://travis-ci.org/ajdawson/eofs/jobs/527713884.
    
    The doctr command that was run is
    
        /home/travis/miniconda/envs/test/bin/doctr deploy dev

--- dev/_static/documentation_options.js
+++ dev/_static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '1.3.1+14.ge95c3d2',
+    VERSION: '1.3.1+16.g402ab38',
     LANGUAGE: 'None',
     COLLAPSE_INDEX: false,
     FILE_SUFFIX: '.html',
