bf669c3991604e7d9b756188807f64a88dc3ff9a
Andrew Dawson <ajdatm@gmail.com>
2024-01-11 18:15:00 +0000

    Remove Python 2/3 compatibility imports

--- lib/eofs/iris.py
+++ lib/eofs/iris.py
@@ -15,8 +15,6 @@
 #
 # You should have received a copy of the GNU General Public License
 # along with eofs.  If not, see <http://www.gnu.org/licenses/>.
-from __future__ import (absolute_import, division, print_function)  # noqa
-
 import collections.abc
 from copy import copy
 
--- lib/eofs/standard.py
+++ lib/eofs/standard.py
@@ -16,7 +16,6 @@
 #
 # You should have received a copy of the GNU General Public License
 # along with eofs.  If not, see <http://www.gnu.org/licenses/>.
-from __future__ import (absolute_import, division, print_function)  # noqa
 import collections.abc
 import warnings
 
